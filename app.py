e5b1c87e8d384696f01731e3b7c3b9714e8e1be6
Namal Jayawardana <namaljayawardana2001@gmail.com>
2025-05-07 18:10:50 +0530

    Initial Commit 04

--- app.py
+++ app.py
@@ -68,5 +68,5 @@ def predict_word_jumbling():
     return make_prediction("Word_Jumbling", request.json)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
 
